7e9cd433cd8c979b308efa4f639acb3390f1f295
lcontreras21 <luiscontrerasorendain@gmail.com>
2025-04-17 16:56:36 -0400

    cmp update for v0.11

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -52,8 +52,55 @@ cmp.setup.cmdline(':', {
         }),
     }),
     sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
+        { name = 'path' },
+        {
+            name = "nvim_lsp",
+            priority = 100,
+            group_index = 1,
+        },
+        { name = "nvim_lsp_signature_help", priority = 100, group_index = 1 },
+        {
+            name = "treesitter",
+            max_item_count = 5,
+            priority = 90,
+            group_index = 5,
+            entry_filter = function(entry, vim_item)
+                if entry.kind == 15 then
+                    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+                    local line = vim.api.nvim_get_current_line()
+                    local next_char = line:sub(cursor_pos[2] + 1, cursor_pos[2] + 1)
+                    if next_char == '"' or next_char == "'" then
+                        vim_item.abbr = vim_item.abbr:sub(1, -2)
+                    end
+                end
+                return vim_item
+            end,
+        },
+        {
+            name = "rg",
+            keyword_length = 5,
+            max_item_count = 5,
+            option = {
+                additional_arguments = "--smart-case --hidden",
+            },
+            priority = 80,
+            group_index = 3,
+        },
+        {
+            name = "buffer",
+            max_item_count = 5,
+            keyword_length = 2,
+            priority = 50,
+            entry_filter = function(entry)
+                return not entry.exact
+            end,
+            option = {
+                get_bufnrs = function()
+                    return vim.api.nvim_list_bufs()
+                end,
+            },
+            group_index = 4,
+        },
         { name = 'cmdline', keyword_pattern = [[\!\@<!\w*]] },
     })
 })
